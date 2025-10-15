b40ca1e59f7cc0402e285655e631aeba918d3549
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-10-15 11:04:59 -0500

    day 0

---
+++ src/main/java/hub/com/guesthemonsterapi/GuesTheMonsterApiApplication.java
@@ -0,0 +1,13 @@
+package hub.com.guesthemonsterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesTheMonsterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuesTheMonsterApiApplication.class, args);
+    }
+
+}
